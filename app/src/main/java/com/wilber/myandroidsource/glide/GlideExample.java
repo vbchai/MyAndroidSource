881a29966f648b7dff38dd805b58187ec1a02b7a
vbchai <vbchai@gmail.com>
2016-04-06 16:45:28 +0800

    Add glide color and style

---
+++ app/src/main/java/com/wilber/myandroidsource/glide/GlideExample.java
@@ -0,0 +1,25 @@
+package com.wilber.myandroidsource.glide;
+
+/**
+ * Created by PMA on 06/04/2016.
+ */
+public class GlideExample {
+    /*
+    Glide.with(this)
+    .load(url)
+    .into(imageview)
+
+
+    //need an bitmap for notification?
+    int largeIconWitdh = Build.VERSION.SDK_INT >= Build.VERSION_CODES>HONEYCOMB ?
+            ? resources.getDimensionPixelSize(android.R.dimen.notification_large_icon_width)
+            : resources.getDimensionPixelSize(R.dimen.notification_large_icon_default); //default = 48dp define it in dimens.xml
+    Bitmap largeIcon = Glide.with(context)
+    .load(url)
+    .asBitmap()
+    .error(errorArtResourceId)
+    .into(largeIconWidth, largeIconHeight)//fixed size
+    .get();
+
+    */
+}
